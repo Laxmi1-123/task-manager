2acfb7a1859074e86a78cb355a32af0dfdb83c96
uit2023860 <ankita.sarvale@cumminscollege.in>
2026-03-24 12:57:22 +0530

    Setup

---
+++ src/test/java/TaskServiceSearchTest.java
@@ -0,0 +1,49 @@
+package test.java;
+import org.junit.jupiter.api.*;
+
+import main.java.Task;
+import main.java.TaskService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
+class TaskServiceSearchTest {
+
+    TaskService service;
+
+    @BeforeEach
+    void setup() {
+
+        service = new TaskService();
+
+        service.addTask(new Task(1,"Study Java",2));
+        service.addTask(new Task(2,"Study DevOps",1));
+        service.addTask(new Task(3,"Play Cricket",3));
+
+        service.markDone(2);
+    }
+
+    @Test
+    void shouldSearchByKeyword() {
+
+        List<Task> result = service.searchByTitle("Study");
+
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void searchShouldBeCaseInsensitive() {
+
+        List<Task> result = service.searchByTitle("java");
+
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void shouldReturnCompletedTasks() {
+
+        List<Task> completed = service.getCompletedTasks();
+
+        assertEquals(1, completed.size());
+    }
+}
